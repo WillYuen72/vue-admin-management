c05e05e35f44d15e0810ebda7b5c1a34fe69ee05
Goffee Ko <goffee.ko@transact24.com>
2019-06-20 15:43:11 +0800

    add simple e2e(cypress) test case

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,17 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+
+    cy.get('a').contains('Demo').click()
+
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/demo')
+    })
+
+    cy.get('.el-card__header div h1').should(($p) => {
+      // refer https://docs.cypress.io/zh-cn/guides/references/assertions.html#Chai-jQuery
+      // expect($p).to.contain('Paged Table')
+      expect($p).to.text('Paged Table')
+    })
   })
 })
